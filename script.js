d044d2c6fec220afe2ee733c7aefee4f6b514428
brunopclaro <67464682+brunopclaro@users.noreply.github.com>
2023-04-26 14:35:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const pdfInput = document.getElementById('pdf-input');
-    const pdfContainer = document.getElementById('pdf-container');
+document.getElementById("convert").addEventListener("click", async function () {
+  const pdfFile = document.getElementById("pdfFile").files[0];
+  if (!pdfFile) {
+    alert("Please select a PDF file.");
+    return;
+  }
 
-    pdfInput.addEventListener('change', async (event) => {
-        const file = event.target.files[0];
-        if (file.type !== 'application/pdf') {
-            alert('Please select a PDF file.');
-            return;
-        }
-
-        // Clear previous content
-        pdfContainer.innerHTML = '';
-
-        // Load PDF
-        const pdfData = await file.arrayBuffer();
-        const pdf = await pdfjsLib.getDocument({ data: pdfData }).promise;
-
-        // Convert each PDF page to HTML
-        for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
-            const page = await pdf.getPage(pageNum);
-            const viewport = page.getViewport({ scale: 1.0 });
-            const canvas = document.createElement('canvas');
-            const ctx = canvas.getContext('2d');
-            canvas.width = viewport.width;
-            canvas.height = viewport.height;
-
-            const renderContext = {
-                canvasContext: ctx,
-                viewport: viewport,
-            };
-
-            await page.render(renderContext).promise;
-            pdfContainer.appendChild(canvas);
-        }
-    });
+  const pdfData = await pdfFile.arrayBuffer();
+  const htmlOutput = document.getElementById("htmlOutput");
+  
+  pdf2htmlEX(pdfData, { worker: false }, function (err, htmlText) {
+    if (err) {
+      console.error(err);
+      alert("Failed to convert the PDF.");
+    } else {
+      htmlOutput.innerHTML = htmlText;
+    }
+  });
 });
+
